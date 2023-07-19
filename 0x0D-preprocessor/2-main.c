a52b37e87ead6dbf334c6981ee770a232e51f422
Itzsammi <bodmas50@gmail.com>
2023-07-18 19:00:44 -0700

    4-sum.h

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main() {
+/**
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main() 
+{
 	printf("Compiled from file: %s\n", __FILE__);
 	
-	return 0;
+	return(0);
 }
