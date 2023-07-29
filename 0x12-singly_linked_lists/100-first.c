87f8245f273496385aa30d40fba1163d734f0823
Itzsammi <bodmas50@gmail.com>
2023-07-28 19:22:22 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@ void __attribute__((constructor)) main(void);
  * main - Prints a string before the
  *        main function is executed.
  */
-void hare(void)
+void main(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 	       "I bore my house upon my back!\n");
