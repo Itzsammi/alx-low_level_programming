e95d1d20b581f67ccd8ee2602f476268b237dc80
Itzsammi <bodmas50@gmail.com>
2023-08-01 03:20:58 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,22 @@
-  1 #include <stdlib.h>
-  2 #include "lists.h"
-  3 #include <stdio.h>
-  4
-  5 /**
-  6   * print_listint - Prints all the elements of a listint_t list.
-  7   * @h: A pointer to the head.
-  8   *
-  9   * Return: The number of nodes.
- 10   */
- 11 size_t print_listint(const listint_t *h)
- 12 {
- 13         size_t nodes = 0;
- 14         while (h)
- 15         {
- 16                 nodes++;
- 17                 printf("%d\n", h->n);
- 18                 h = h->next;
- 19         }
- 20         return (nodes);
- 21 }
+#include <stdlib>
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: A pointer to the head.
+ *
+ * Return: The number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (nodes);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,36 +1,37 @@
-  1 #ifndef LIST_H
-  2 #define LIST_H
-  3
-  4 #include <stdlib.h>
-  5
-  6 /**
-  7  * struct listint_s - singly linked list
-  8  * @n: integer
-  9  * @next: points to the next node
- 10  *
- 11  * Description: singly linked list node structure
- 12  *
- 13  */
- 14 typedef struct listint_s
- 15 {
- 16             int n;
- 17                 struct listint_s *next;
- 18 } listint_t;
- 19
- 20 size_t print_listint(const listint_t *h);
- 21 size_t listint_len(const listint_t *h);
- 22 listint_t *add_nodeint(listint_t **head, const int n);
- 23 listint_t *add_nodeint_end(listint_t **head, const int n);
- 24 void free_listint(listint_t *head);
- 25 void free_listint2(listint_t **head);
- 26 int pop_listint(listint_t **head);
- 27 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
- 28 int sum_listint(listint_t *head);
- 29 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
- 30 int delete_nodeint_at_index(listint_t **head, unsigned int index);
- 31 listint_t *reverse_listint(listint_t **head);
- 32 size_t print_listint_safe(const listint_t *head);
- 33 size_t free_listint_safe(listint_t **h);
- 34 listint_t *find_listint_loop(listint_t *head);
- 35
- 36 #endif
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
+
+#endif
