a3766f0616e8efd5b1c0bd251ed1f413b7cc43b8
Itzsammi <bodmas50@gmail.com>
2023-08-31 17:23:05 -0700

    new tasks

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,13 +5,14 @@
  * @s: string to be printed
  */
 
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+
 	else
 		_putchar('\n');
 }
