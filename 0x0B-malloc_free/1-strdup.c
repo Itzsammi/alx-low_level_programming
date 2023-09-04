915f6600dd1e7b72c93ed7fabf269f22fb5b65ba
Itzsammi <bodmas50@gmail.com>
2023-09-04 12:05:11 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+
+/**
+  * _strdup - Duplicate a string
+  * @str: the string to duplicate
+  *
+  * Return: the string duplicated
+  */
+char *_strdup(char *str)
+{
+	int w = 0, i = 1;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+	{
+		i++;
+	}
+
+	s = malloc((sizeof(char) * i) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	while (w < i)
+	{
+		s[w] = str[w];
+		w++;
+	}
+
+	s[w] = '\0';
+	return (s);
+}
